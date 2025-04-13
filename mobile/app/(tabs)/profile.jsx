953c8f5d8f2becbff2f368e34e644279a9b506a6
Yassine <ymeftah777@gmail.com>
2025-04-13 18:54:58 +0100

    feat: add profile and settings screens with tab navigation

---
+++ mobile/app/(tabs)/profile.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function ProfileScreen() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-lg font-bold">Profile Page</Text>
+    </View>
+  );
+}
