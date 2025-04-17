4de525497d1e7bc754a772012ada68d23bfa8b14
Yassine <ymeftah777@gmail.com>
2025-04-17 22:43:20 +0100

    fix: update API_URL import and endpoint for GROQ chat service

--- mobile/components/groqService.jsx
+++ mobile/components/groqService.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-// import { API_URL } from '@env' ;
+import { API_URL } from '../ApiConfig'; // Import the API URL from config
 
-const API_URL = 'http://192.168.000.105:8000/api/groq-chat'; // Replace with your Laravel API URL
 
 export const askGroq = async (userMessage) => {
   try {
-    const response = await axios.post(API_URL, { message: userMessage });
+    const response = await axios.post(`${API_URL}/groq-chat`, { message: userMessage });
     return response.data.choices?.[0]?.message?.content;
   } catch (error) {
     console.error('Error calling Laravel + GROQ:', error);
