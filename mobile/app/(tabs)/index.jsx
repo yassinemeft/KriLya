ed0cbd59e061bac6281639945d6d16c560261fd0
Yassine <ymeftah777@gmail.com>
2025-04-17 09:12:32 +0100

    fix: update API endpoint in HomeScreen component

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -9,7 +9,7 @@ export default function HomeScreen() {
   const [text, setText] = useState('');
 
   useEffect(() => {
-    axios.get('http://192.168.100.187:8000/api/test')
+    axios.get('http://192.168.000.105:8000/api/test')
       .then(response => {
         setMessage(response.data.message);
         setLoading(false);
