7b4e24efe068a865e5aad487c8b2c79fe9313785
Yassine <ymeftah777@gmail.com>
2025-05-19 23:37:52 +0100

    feat: implement language toggle functionality and refactor layout structure

--- mobile/app/(tabs)/_layout.web.jsx
+++ mobile/app/(tabs)/_layout.web.jsx
@@ -1,5 +1,6 @@
 import { Slot, Tabs } from "expo-router";
-import React from "react";
+import * as React from "react";
+import { useState } from "react";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Button, ButtonText } from "@/components/ui/button";
 import { Divider } from "@/components/ui/divider";
@@ -10,7 +11,8 @@ import { Link, LinkText } from "@/components/ui/link";
 import { Icon, SunIcon, MoonIcon, MenuIcon } from "@/components/ui/icon";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useTheme } from "@/components/theme/ThemeProvider";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { useTranslation } from "react-i18next";
 
 import "../../i18n"; // Load translations
 import { I18nextProvider } from "react-i18next";
@@ -21,92 +23,70 @@ export default function TabLayout() {
 
   const { theme, toggleTheme } = useTheme();
 
+  const { t, i18n } = useTranslation();
+  const [locale, setLocale] = useState(
+    i18n.language.startsWith("ar") ? "ar" : "fr"
+  );
+
+  const toggleLanguage = () => {
+    const localeMap = {
+      en: "ar",
+      ar: "fr",
+      fr: "en",
+    };
+    const newLocale = localeMap[locale];
+    setLocale(newLocale);
+    i18n.changeLanguage(newLocale); // au lieu de i18n.locale = ...
+  };
+
   return (
-    <View>
-    <HStack
-      className="flex flex-row items-center justify-between h-14 px-7
-      bg-brandBlue-500 text-white dark:bg-black dark:text-brandRed-500 shadow"
-    >
-      <Image
-        source={require("@/assets/images/logo.png")}
-        alt="KriLya Logo"
-        className="hidden sm:flex w-full max-w-[200px]"
-        resizeMode="contain"
-        size="xs"
-      />
-      <Icon className="sm:hidden flex" as={MenuIcon} />
-      <Button onPress={toggleTheme}>
-        <ButtonText>{theme === "light" ? <Icon className="text-typography-500" as={SunIcon} /> : <Icon className="text-typography-500" as={MoonIcon} />}</ButtonText>
-      </Button>
-
-      <HStack className="space-x-2 hidden sm:flex items-center">
-        <Button variant="link" size="xs">
-          <ButtonText>Github</ButtonText>
+    <ScrollView>
+      <HStack
+        className="flex flex-row items-center justify-between h-14 px-7
+      bg-brandBlue-500 text-white dark:bg-black dark:text-brandGreen-500 shadow"
+      >
+        <Image
+          source={require("@/assets/images/logo.png")}
+          alt="KriLya Logo"
+          className="hidden sm:flex w-full max-w-[200px]"
+          resizeMode="contain"
+          size="xs"
+        />
+        <Icon className="sm:hidden flex" as={MenuIcon} />
+        <Button onPress={toggleTheme}>
+          <ButtonText>
+            {theme === "light" ? (
+              <Icon className="text-typography-500" as={SunIcon} />
+            ) : (
+              <Icon className="text-typography-500" as={MoonIcon} />
+            )}
+          </ButtonText>
         </Button>
-        <Divider orientation="vertical" className="hidden mx-2.5" />
-        <Button variant="link" size="xs">
-          <ButtonText>Twitter</ButtonText>
+        <Button  onPress={toggleLanguage}>
+          <ButtonText>{t("switch_language")}</ButtonText>
         </Button>
-      </HStack>
 
-      <HStack className="space-x-2 hidden sm:flex items-center">
-        <Link href="/login">
-          <LinkText>Sign in</LinkText>
-        </Link>
-        <Divider orientation="vertical" className="mx-2.5" />
-        <Link href="/register">
-          <LinkText>Sign up</LinkText>
-        </Link>
-      </HStack>
-    </HStack>
+        <HStack className="space-x-2 hidden sm:flex items-center">
+          <Button variant="link" size="xs">
+            <ButtonText>Github</ButtonText>
+          </Button>
+          <Divider orientation="vertical" className="hidden mx-2.5" />
+          <Button variant="link" size="xs">
+            <ButtonText>Twitter</ButtonText>
+          </Button>
+        </HStack>
 
+        <HStack className="space-x-2 hidden sm:flex items-center">
+          <Link href="/login">
+            <LinkText>Sign in</LinkText>
+          </Link>
+          <Divider orientation="vertical" className="mx-2.5" />
+          <Link href="/register">
+            <LinkText>Sign up</LinkText>
+          </Link>
+        </HStack>
+      </HStack>
       <Slot />
-
-
-    </View>
+    </ScrollView>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<VStack className="sm:hidden bg-white px-4 py-2 shadow">
-  <Button variant="link" size="sm" onPress={() => setIsMenuOpen(false)}>
-    <ButtonText>Github</ButtonText>
-  </Button>
-  <Button variant="link" size="sm" onPress={() => setIsMenuOpen(false)}>
-    <ButtonText>Twitter</ButtonText>
-  </Button>
-  <Button variant="link" size="sm" onPress={() => setIsMenuOpen(false)}>
-    <ButtonText>Discord</ButtonText>
-  </Button>
-</VStack>;
-
-<Image
-  source={require("@/assets/images/logo.png")}
-  alt="KriLya Logo"
-  className="hidden sm:flex w-full"
-  resizeMode="contain"
-  size="2xs"
-/>;
