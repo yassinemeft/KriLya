c3bb935ad265cdda19fdf5f4830c122d3ffff6ab
AYOUB-41 <168175419+AYOUB-41@users.noreply.github.com>
2025-05-20 22:30:19 +0100

    feat: refactor login and signup screens with new UI components and improved state management

--- mobile/app/(tabs)/signup.jsx
+++ mobile/app/(tabs)/signup.jsx
@@ -6,6 +6,7 @@ import { Input, InputField } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
+
 export default function Signup() {
   const [form, setForm] = useState({
     name: "",
@@ -25,83 +26,85 @@ export default function Signup() {
 
   return (
     <View>
-      {/* big screen */}
-      <Box className="w-full max-w-md self-center bg-gray-800  p-8 shadow-lg mt-10">
-        <Heading className="text-center text-2xl font-bold mb-6 text-white">
+      <Box className="w-full max-w-md self-center bg-white dark:bg-gray-800 p-8 shadow-lg mt-10 rounded-xl">
+        <Heading className="text-center text-2xl font-bold mb-6 text-gray-800 dark:text-white">
           Créer un compte
         </Heading>
         <VStack space="6" className="items-center">
-          <Text className="text-blue-200 text-center mb-4 mt-4 font-bold">
+          <Text className="text-blue-600 dark:text-blue-200 text-center mb-4 mt-4 font-bold">
             Nom :
           </Text>
           <Input
             value={form.name}
             onChangeText={(val) => handleChange("name", val)}
-            className="w-72 bg-gray-900 border border-gray-600 rounded text-white px-4 "
+            className="w-72 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 rounded text-gray-800 dark:text-white px-4 placeholder:text-gray-500 placeholder:text-opacity-50"
           >
             <InputField type="text" placeholder="Votre Nom" />
           </Input>
-          <Text className="text-blue-200 text-center mb-4 mt-4 font-bold">
+
+          <Text className="text-blue-600 dark:text-blue-200 text-center mb-4 mt-4 font-bold">
             Prénom :
           </Text>
           <Input
-            value={form.name}
+            value={form.lastName}
             onChangeText={(val) => handleChange("lastName", val)}
-            className="w-72 bg-gray-900 border border-gray-600 rounded text-white px-4 "
+            className="w-72 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 rounded text-gray-800 dark:text-white px-4 placeholder:text-gray-500 placeholder:text-opacity-50"
           >
-            <InputField type="text" placeholder="Votre Prenom" />
+            <InputField type="text" placeholder="Votre Prénom" />
           </Input>
 
-          <Text className="text-blue-200 text-center mb-4 mt-4 font-bold">
+          <Text className="text-blue-600 dark:text-blue-200 text-center mb-4 mt-4 font-bold">
             Email :
           </Text>
           <Input
             keyboardType="email-address"
             value={form.email}
             onChangeText={(val) => handleChange("email", val)}
-            className="w-72 bg-gray-900 border border-gray-600 rounded text-white px-4 "
+            className="w-72 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 rounded text-gray-800 dark:text-white px-4 placeholder:text-gray-500 placeholder:text-opacity-50"
           >
             <InputField type="email" placeholder="Votre Email" />
           </Input>
 
-          <Text className="text-blue-200 text-center mb-4 mt-4 font-bold">
+          <Text className="text-blue-600 dark:text-blue-200 text-center mb-4 mt-4 font-bold">
             Mot de passe :
           </Text>
           <Input
             placeholder="Mot de passe"
-            placeholderTextColor="#ccc"
+            placeholderTextColor="rgba(153,153,153,0.5)" // opacité plus faible
             secureTextEntry
             value={form.password}
             onChangeText={(val) => handleChange("password", val)}
-            className="w-72 bg-gray-900 border border-gray-600 rounded text-white px-4"
+            className="w-72 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 rounded text-gray-800 dark:text-white px-4"
           >
             <InputField type="password" placeholder="Votre Mot de passe" />
           </Input>
-          <Text className="text-blue-200 text-center mb-4 mt-4 font-bold">
+
+          <Text className="text-blue-600 dark:text-blue-200 text-center mb-4 mt-4 font-bold">
             Confirmer le mot de passe :
           </Text>
           <Input
             secureTextEntry
-            value={form.password}
+            value={form.confirmPassword}
             onChangeText={(val) => handleChange("confirmPassword", val)}
-            className="w-72 bg-gray-900 border border-gray-600 rounded text-white px-4"
+            className="w-72 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-600 rounded text-gray-800 dark:text-white px-4"
           >
             <InputField
               type="password"
               placeholder="Confirmer votre mot de passe"
+              placeholderTextColor="rgba(153,153,153,0.5)"
             />
           </Input>
 
           <Button
             onPress={handleSignUp}
-            className="bg-blue-600 rounded mt-4 w-72 data-[hover=true]:bg-brandRed-300 dark:data-[hover=true]:bg-brandGreen-300"
+            className="bg-brandRed-500 dark:bg-brandGreen-400 hover:bg-blue-700 dark:hover:bg-brandGreen-500 rounded mt-4 w-72"
           >
-            <Text className="text-white font-semibold text-center ">
+            <Text className="text-white font-semibold text-center">
               S’inscrire
             </Text>
           </Button>
         </VStack>
-        <Text className="text-center text-gray-400 mt-6">
+        <Text className="text-center text-gray-600 dark:text-gray-400 mt-6">
           Déjà un compte ? Connecte-toi.
         </Text>
       </Box>
