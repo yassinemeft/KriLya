8d1778d3651f5408b4505ebd0cf6deedfa4b17dc
Yassine <ymeftah777@gmail.com>
2025-05-23 15:36:57 +0100

    feat: create migration for house_images table with foreign key to houses

---
+++ backend-laravel/database/migrations/2025_05_22_232936_create_house_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('house_images', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('house_id')->constrained('houses')->onDelete('cascade');
+            $table->string('url'); // URL of the image, e.g. Cloudinary URL
+            $table->boolean('is_main')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('house_images');
+    }
+};
